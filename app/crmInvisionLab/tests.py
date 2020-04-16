689b80f7312ebfff5f32c6ea8b7899fab38604dd
roberta <roberta.mangiapane@gmail.com>
2020-04-16 18:29:06 +0100

    feature test passing

--- app/crmInvisionLab/tests.py
+++ app/crmInvisionLab/tests.py
@@ -35,39 +35,45 @@ class FeatureTestInfrastructure(TestCase):
 
         self.assertIn("Added collaborator", response_text)
 
-    #
-    # def test_added_collaborator_redirect_to_home(self):
-    #     response = self.client.post('/add/', {'name': "Added collaborator", 'email': "email", 'phone': "5555555555",
-    #                                           'position': "Rome", 'availability': "Weekend",
-    #                                           'main_skills': "One skill",
-    #                                           'secondary_skills': "Two skill"})
-    #
-    #     self.assertRedirects(response, '/')
 
-    # def test_user_cannot_add_collaborator_without_name(self):
-    #     with self.assertRaises(ValidationError):
-    #         self.client.post('/add/', {'name': "", 'email': "email", 'phone': "5555555555",
-    #                                    'position': "Rome", 'availability': "Weekend",
-    #                                    'main_skills': "One skill",
-    #                                    'secondary_skills': "Two skill"})
+    def test_added_collaborator_redirect_to_home(self):
+        skill = Skill("1", "Compositing")
+        skill.save()
 
-    # def test_view_collaborator_page(self):
-    #     self.client.post('/add/', {'name': "Added collaborator", 'email': "email", 'phone': "5555555555",
-    #                                'position': "Rome", 'availability': "Weekend",
-    #                                'main_skills': "One skill", 'secondary_skills': "Two skill"})
-    #     collaborator = Collaborator.objects.get(name="Added collaborator")
-    #     collaborator_page = self.client.get('/collaborator/' + str(collaborator.id))
-    #
-    #     response_text = collaborator_page.content.decode("utf-8")
-    #
-    #     self.assertIn("Added collaborator", response_text)
+        response = self.client.post('/add/', {'name': "Added collaborator", 'email': "email", 'phone': "5555555555",
+                                              'position': "Rome", 'availability': "Weekend", 'main_skill': "Compositing"})
+
+        self.assertRedirects(response, '/')
+
+    def test_user_cannot_add_collaborator_without_name(self):
+        skill = Skill("1", "Compositing")
+        skill.save()
+
+        with self.assertRaises(ValidationError):
+            self.client.post('/add/', {'name': "", 'email': "email", 'phone': "5555555555",
+                                       'position': "Rome", 'availability': "Weekend",
+                                       'main_skill': "Compositing"})
+
+    def test_view_collaborator_page(self):
+        skill = Skill("1", "Compositing")
+        skill.save()
+
+        self.client.post('/add/', {'name': "Added collaborator", 'email': "email", 'phone': "5555555555",
+                                   'position': "Rome", 'availability': "Weekend",
+                                   'main_skill': "Compositing"})
+        collaborator = Collaborator.objects.get(name="Added collaborator")
+        collaborator_page = self.client.get('/collaborator/' + str(collaborator.id))
+
+        response_text = collaborator_page.content.decode("utf-8")
+
+        self.assertIn("Added collaborator", response_text)
 
 
-    # def test_edit_collaborators(self):
-    #     response = self.client.get('/edit/')
-    #
-    #     self.assertEqual(response.status_code, 200)
-    #     self.assertEqual(response.content, b"Edit a selected collaborator")
+    def test_edit_collaborators(self):
+        response = self.client.get('/edit/')
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.content, b"Edit a selected collaborator")
 
     # def test_user_input_wrong_date_format(self):
     # string or wrong date
