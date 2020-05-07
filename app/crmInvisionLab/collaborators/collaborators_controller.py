058703ffdf045bdd9d4b55b64acadaa9a16eda3f
roberta <roberta.mangiapane@gmail.com>
2020-05-07 18:26:53 +0100

    WIP boostrap, refactored collaborator delete and update view

--- app/crmInvisionLab/collaborators/collaborators_controller.py
+++ app/crmInvisionLab/collaborators/collaborators_controller.py
@@ -1,6 +1,6 @@
 from ..models import Collaborator
 from django.views.generic.list import ListView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from django.views.generic.detail import DetailView
 from .collaborator_form import AddCollaboratorForm, SearchCollaboratorForm
 from .collaborators_filtering_helpers import *
@@ -17,7 +17,7 @@ class CollaboratorList(ListView):
         if collaborator_search_form.is_valid() and self.request.GET:
             sort_params = collaborator_filter(collaborator_search_form.cleaned_data)
 
-        return Collaborator.objects.filter(**sort_params)
+        return Collaborator.objects.filter(**sort_params).order_by(self.ordering)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -56,4 +56,30 @@ class CollaboratorView(DetailView):
         context['collaborator'] = self.object
         return context
 
+
+class CollaboratorDelete(DeleteView):
+    model = Collaborator
+    success_url = '/collaborators'
+
+
+class CollaboratorUpdate(UpdateView):
+    model = Collaborator
+    fields = AddCollaboratorForm.Meta.fields
+    template_name_suffix = '_update_form'
+
+    def get_form(self, form_class=None):
+        form = super().get_form(form_class)
+        form.fields['name'].widget.attrs.update({'class': 'form-control'})
+        form.fields['email'].widget.attrs.update({'class': 'form-control'})
+        form.fields['phone'].widget.attrs.update({'class': 'form-control'})
+        form.fields['position'].widget.attrs.update({'class': 'form-control'})
+        form.fields['availability'].widget.attrs.update({'class': 'form-control'})
+        form.fields['main_skills'].widget.attrs.update({'class': 'form-control'})
+        form.fields['secondary_skills'].widget.attrs.update({'class': 'form-control'})
+        return form
+
+    def form_valid(self, form):
+        self.object.save()
+        self.success_url = '/collaborators/view/' + str(self.object.id)
+        return super().form_valid(form)
 
